e8b41a5c1d1d1926c6dff25701b019d4259af8bf
Admin <Admin@DESKTOP-9VS7AV9>
2023-05-03 14:11:15 +0800

    hiisen dasgal

---
+++ immutable/src/immutable/Course.java
@@ -0,0 +1,17 @@
+package immutable;
+
+public class Course {
+	String courseName;
+	Course(String courseName) {
+		this.courseName=courseName;
+	}
+public String getCourseName() {
+	return courseName=courseName;
+}
+public void setCourseName(String courseName) {
+	this.courseName=courseName;
+}
+}
+
+	
+
